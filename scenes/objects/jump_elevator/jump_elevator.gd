905b1673cf7a43df1a7b778e3837a8eda89a273c
Student-46-2021 <Student-46-2021@rtk.local>
2022-12-12 11:18:43 +0300

    upd

---
+++ scenes/objects/jump_elevator/jump_elevator.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+@export var jump_force = 10.0
+
+func _on_area_3d_body_entered(body):
+	if body is Player:
+		var player = body as  Player
+		player.jump(jump_force)
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,10 +1,11 @@
 extends CharacterBody3D
+class_name Player
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 6.0
 const JUMP_RELEASE = 2.0
 const MASS = 1.0
-const FRICTION = 10
+const FRICTION = 10.0
 
 @onready var head = $Head as Head
 
@@ -13,7 +14,6 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		self.velocity.y -= gravity * MASS *  delta
 	else:
@@ -22,14 +22,7 @@ func _physics_process(delta):
 
 	# Handle Jump. Holding jump key longer make jump higher
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		self.velocity.y = JUMP_VELOCITY
-		
-		var jump_dir = head.get_jump_direction()
-		jump_dir *= SPEED
-		
-		self.velocity.x = jump_dir.x
-		self.velocity.z = jump_dir.z
-			
+		jump(JUMP_VELOCITY)
 		
 	if Input.is_action_just_released("jump") and not is_on_floor() and velocity.y > 0:
 		self.velocity.y /= JUMP_RELEASE
@@ -38,3 +31,10 @@ func _physics_process(delta):
 	
 	move_and_slide()
 	
+
+func jump(jump_velocity : float):
+	self.velocity.y = jump_velocity
+	
+	var jump_direction = head.get_jump_direction()
+	self.velocity.x = jump_direction.x * SPEED
+	self.velocity.z = jump_direction.z * SPEED
