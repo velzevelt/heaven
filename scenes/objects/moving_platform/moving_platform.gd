1049c600e5ad15fe4cd62349ae41bad3549ed542
velz <velz@velz>
2023-01-03 20:46:20 +0300

    upd

--- scenes/objects/moving_platform/moving_platform.gd
+++ scenes/objects/moving_platform/moving_platform.gd
@@ -1,6 +1,15 @@
 extends Node3D
 
 @onready var anim_player = $AnimationPlayer as AnimationPlayer
+@export var autoplay := true
+@export var loop := true
+
 
 func _ready():
-	anim_player.play('move_platform')
+	if autoplay:
+		anim_player.play('move_platform')
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == 'move_platform' and loop:
+		anim_player.play('move_platform')
