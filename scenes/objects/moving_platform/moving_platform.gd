ddafb74b82238e7f1f95cbf51ba7ee8e7f7fd1c8
velz <velz@velz>
2023-01-06 17:45:12 +0300

    upd

--- scenes/objects/moving_platform/moving_platform.gd
+++ scenes/objects/moving_platform/moving_platform.gd
@@ -1,16 +1,22 @@
 class_name MovingPlatform extends Node3D
 
-@onready var anim_player = $AnimationPlayer as AnimationPlayer
+@export var path_curve : Curve3D
 @export var autoplay := true
 @export var loop := true
 
+@onready var anim_player = $AnimationPlayer as AnimationPlayer
+@onready var path = $Path3D as Path3D
 
 func _ready():
 	if autoplay:
-		anim_player.play('move_platform')
+		move_platform()
 	
+	path.curve = path_curve
 
+func move_platform():
+	anim_player.play('move_platform')
+	
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == 'move_platform' and loop:
-		anim_player.play('move_platform')
+		move_platform()
