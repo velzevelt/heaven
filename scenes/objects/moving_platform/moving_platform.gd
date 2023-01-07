ea4be2a7a292d0133d1c8fec1d45a963c2ef572f
velz <velz@velz>
2023-01-07 15:40:25 +0300

    upd

--- scenes/objects/moving_platform/moving_platform.gd
+++ scenes/objects/moving_platform/moving_platform.gd
@@ -9,17 +9,9 @@ signal move_ended
 
 @onready var path_follow : PathFollow3D = $Path3D/PathFollow3D
 
-var tween
-var init_progress := 0.0
-var final_progress := 1.0
-
 
 func _ready():
 	super()
-	
-	tween = create_tween()
-	tween.stop()
-	tween.finished.connect(_on_tween_finished)
 	
 	if autoplay:
 		move_platform()
@@ -30,19 +22,18 @@ func _on_player_entered():
 
 
 func move_platform():
-	if !tween.is_running():
-		var progress_ratio = final_progress if path_follow.progress_ratio == init_progress else init_progress
-		Logger.debug_log(progress_ratio)
-		tween = create_tween()
-		tween.stop()
-		tween.tween_property(path_follow, 'progress_ratio', progress_ratio, anim_duration)
-		tween.play()
-		move_started.emit()
+	var tween = create_tween()
+	tween.stop()
+	tween.tween_property(path_follow, 'progress_ratio', 1.0, anim_duration)
+	
+	if not loop:
+		tween.finished.connect(func(): move_ended.emit())
 	else:
-		Logger.debug_log('121')
+		tween.tween_property(path_follow, 'progress_ratio', 0.0, anim_duration)
+		tween.set_loops()
+	
+	tween.play()
+	move_started.emit()
 
-func _on_tween_finished():
-	if loop:
-		move_platform()
-	else:
-		move_ended.emit()
+
+
