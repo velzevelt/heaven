96c6da1eabfe48f6b312ffa1fa397b917c19cdd1
Student-126 <Student-126@rtk.local>
2023-03-14 12:28:05 +0300

    huge success in hook component

--- scenes/objects/player/grappling_hook_component.gd
+++ scenes/objects/player/grappling_hook_component.gd
@@ -22,18 +22,25 @@ func _physics_process(delta):
 		
 		# YOU CAN'T ADD JOINTS IN RUNTIME
 		# That's why need to use PackedScene with Objects and Joint
+		# You can delete joints in runtime
 		
 		var instance = hook.instantiate() as Node3D
 		var rb = instance.get_node('RigidBody3D') as RigidBody3D
 		var sb = instance.get_node('StaticBody3D') as StaticBody3D
 		var joint = instance.get_node('PinJoint3D') as PinJoint3D
+		joint.node_a = ''
+		joint.node_b = ''
 		
 		sb.add_collision_exception_with(body)
 		rb.add_collision_exception_with(body)
 		sb.position = hit_point
 		joint.position = sb.position
-		rb.position = body.global_position
-		rb.position.y += 0.2
+		rb.position = $%Head.global_position
 		
 		body.get_parent().call_deferred('add_child', instance)
-		await rb.tree_entered
+		await rb.tree_entered and sb.tree_entered
+		rb.apply_central_impulse(hit_direction * base_pull_force)
+		
+		await get_tree().create_timer(0.4).timeout
+		joint.node_a = rb.get_path()
+		joint.node_b = sb.get_path()
