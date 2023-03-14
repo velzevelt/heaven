11149c41aef0f2cf8e8964231111cccb304f6371
Student-126 <Student-126@rtk.local>
2023-03-14 12:35:00 +0300

    upd

--- scenes/objects/player/grappling_hook_component.gd
+++ scenes/objects/player/grappling_hook_component.gd
@@ -1,13 +1,11 @@
 extends Node
 
 @export var hook_raycast: RayCast3D
+@export var hook: PackedScene
 @export var body: CharacterBody3D
 @export var velocity_component: VelocityComponent
-@export var player_move_component: PlayerMoveComponent
-@export var elasticity: float = 1.0
 @export var base_pull_force: float = 15.0
-@export var detachment_distance: float = 1.5
-@export var hook: PackedScene
+@export var free_fly_time := 0.4
 #@export var input_component: GrapplingHookInputComponent
 
 
@@ -21,21 +19,23 @@ func _physics_process(delta):
 		var hit_direction = body.global_position.direction_to(hit_point)
 		
 		# YOU CAN'T ADD JOINTS IN RUNTIME
-		# That's why need to use PackedScene with Objects and Joint
+		# That's have to use PackedScene with Objects and Joint
 		# You can delete joints in runtime
 		
 		var instance = hook.instantiate() as Node3D
 		var rb = instance.get_node('RigidBody3D') as RigidBody3D
 		var sb = instance.get_node('StaticBody3D') as StaticBody3D
 		var joint = instance.get_node('PinJoint3D') as PinJoint3D
+		
+		# Clear nodepath to attach bodies it later
 		joint.node_a = ''
 		joint.node_b = ''
 		
 		sb.add_collision_exception_with(body)
 		rb.add_collision_exception_with(body)
 		sb.position = hit_point
-		joint.position = sb.position
-		rb.position = $%Head.global_position
+		joint.position = hit_point
+		rb.position = body.global_position + Vector3.UP # Need to add offset to not stuck on floor after spawn
 		
 		body.get_parent().call_deferred('add_child', instance)
 		await rb.tree_entered and sb.tree_entered
