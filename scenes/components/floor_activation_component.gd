fd0fbff5c747f0ab0957448cd55b2cf82c1178a8
Class_Student <Class_Student@rtk.local>
2023-03-14 13:20:56 +0300

    upd

--- scenes/components/floor_activation_component.gd
+++ scenes/components/floor_activation_component.gd
@@ -5,18 +5,6 @@ extends Node
 @export var body: CharacterBody3D
 var _prev_floor = null
 
-func _ready():
-	update_configuration_warnings()
-
-func _notification(what):
-	match what:
-		NOTIFICATION_PARENTED:
-			update_configuration_warnings()
-		NOTIFICATION_UNPARENTED:
-			update_configuration_warnings()
-
-
-
 
 func _physics_process(_delta):
 	if not Engine.is_editor_hint():
@@ -25,7 +13,7 @@ func _physics_process(_delta):
 			if is_instance_valid(floor):
 				floor = floor.get_collider()
 				if floor != _prev_floor:
-					print("IN STRICT")
+#					print("IN STRICT")
 					if floor.has_signal('object_entered_strict') or floor.has_user_signal('object_entered_strict'):
 						floor.emit_signal('object_entered_strict', body)
 					if floor.has_signal('object_entered') or floor.has_user_signal('object_entered'):
@@ -36,47 +24,14 @@ func _physics_process(_delta):
 			if is_instance_valid(floor):
 				floor = floor.get_collider()
 				if floor != _prev_floor:
-					print("IN")
+#					print("IN")
 					if floor.has_signal('object_entered') or floor.has_user_signal('object_entered'):
 						floor.emit_signal('object_entered', body)
 				_prev_floor = floor
 		else:
 			if is_instance_valid(_prev_floor):
-				print("OUT")
+#				print("OUT")
 				if _prev_floor.has_signal('object_entered') or _prev_floor.has_user_signal('object_entered'):
 					_prev_floor.emit_signal('object_exited', body)
 				_prev_floor = null
 
-
-#if body.is_on_floor():
-#			var floor = body.get_last_slide_collision()
-#			if is_instance_valid(floor):
-#				floor = floor.get_collider()
-#				if floor != _prev_floor:
-#					print("IN")
-#					if floor.has_signal('object_entered') or floor.has_user_signal('object_entered'):
-#						floor.emit_signal('object_entered', body)
-#				_prev_floor = floor
-
-
-
-#		if not body.is_on_floor():
-#			if is_instance_valid(_prev_floor):
-#				print("OUT")
-#				if _prev_floor.has_signal('object_entered') or _prev_floor.has_user_signal('object_entered'):
-#					_prev_floor.emit_signal('object_exited', body)
-#				_prev_floor = null
-#		else:
-#			var floor = body.get_last_slide_collision()
-#			if is_instance_valid(floor):
-#				floor = floor.get_collider()
-#				if floor != _prev_floor:
-#					print("IN")
-#					if body.is_on_floor():
-#						if floor.has_signal('object_entered') or floor.has_user_signal('object_entered'):
-#							floor.emit_signal('object_entered', body)
-#					elif body.is_on_floor_only():
-#						if floor.has_signal('object_entered_strict') or floor.has_user_signal('object_entered_strict'):
-#							floor.emit_signal('object_entered_strict', body)
-#
-#				_prev_floor = floor
