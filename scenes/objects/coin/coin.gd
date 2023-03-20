98ff664c2a5448245a834a88e8df9a6dbff67fc4
Class_Student <Class_Student@rtk.local>
2023-03-20 10:30:24 +0300

    upd

--- scenes/objects/coin/coin.gd
+++ scenes/objects/coin/coin.gd
@@ -9,10 +9,10 @@ func _ready():
 
 
 func _on_area_3d_body_entered(_body):
-	picked_up.emit()
 	Events.object_picked_up.emit(self)
+	picked_up.emit()
 
 
 func _on_pickup():
 	Logger.debug_log("I'm a happy coin")
-	queue_free()
+	call_deferred('queue_free')
---
+++ scenes/objects/map_goal/objective/collect.gd
@@ -0,0 +1,24 @@
+extends Node
+
+@export var target_group: String
+var target_count := 0
+var max_target_count := 0
+
+func _ready():
+	get_tree().node_added.connect(_on_node_added)
+	Events.object_picked_up.connect(_on_object_picked_up)
+	max_target_count = get_tree().get_nodes_in_group(target_group).size()
+
+
+func _on_node_added(node):
+	if node.is_in_group(target_group):
+		max_target_count += 1
+
+
+func _on_object_picked_up(obj):
+	if obj.is_in_group(target_group):
+		target_count += 1
+		
+		if target_count == max_target_count:
+			Logger.debug_log('All collected!')
+
