cb86bdf2a7cb5fe68b8afc8a26dd213c57db05be
Class_Student <Class_Student@rtk.local>
2023-03-20 10:08:25 +0300

    upd

--- scenes/objects/coin/coin.gd
+++ scenes/objects/coin/coin.gd
@@ -1,12 +1,16 @@
-class_name Coin extends Node3D
+class_name Coin 
+extends Node3D
 
 signal picked_up
+
 
 func _ready():
 	self.picked_up.connect(_on_pickup)
 
+
 func _on_area_3d_body_entered(_body):
 	picked_up.emit()
+
 
 func _on_pickup():
 	Logger.debug_log("I'm a happy coin")
--- scenes/objects/map_goal/map_goal.gd
+++ scenes/objects/map_goal/map_goal.gd
@@ -5,3 +5,6 @@ extends Node
 @export_multiline var goal_description: String = '' # e.g. You need to collect %d coins from this level
 @export var completed := false
 @export var reward: Script
+
+
+# 
