a7d778c7722439b642394857c5ee890945e18f05
Student-126 <Student-126@rtk.local>
2023-03-27 12:03:52 +0300

    upd

--- scenes/maps/heaven/tutorial/walk/step1.gd
+++ scenes/maps/heaven/tutorial/walk/step1.gd
@@ -1,8 +1,25 @@
 extends Node3D
 
 @export var finish: Node
+@export var message: Label
+@export var enabled := true
 
 func _on_area_3d_body_entered(body):
-	if body.is_in_group('player'):
+	if body.is_in_group('player') and enabled:
 		finish.visible = true
 		finish.enabled = true
+		visible = false
+		enabled = false
+		
+		message.visible_ratio = 0.0
+		message.visible = true
+		var tween = get_tree().create_tween()
+		tween.tween_property(message, 'visible_ratio', 1.0, 2.0)
+		
+		await tween.finished
+		await get_tree().create_timer(2.0).timeout
+		tween = get_tree().create_tween()
+		tween.tween_property(message, 'visible_ratio', 0.0, 2.0)
+		
+		await tween.finished
+		message.visible = false
