2a5f5c432a5013cfaa7a1f2c201ee156bbeeaf5e
Student-126 <Student-126@rtk.local>
2023-03-27 12:37:40 +0300

    upd

--- scenes/map_data.gd
+++ scenes/map_data.gd
@@ -76,4 +76,4 @@ var MAP_SCENE_FILE_NAME = SceneLoader.MAP_SCENE_FILE_NAME # unified name 'map.ts
 ## Determinates position in LevelSelector. 0 - first map, 1 - second, etc
 @export_range(0, 10, 1) var show_order := 0
 
-
+@export var map_tags: PackedStringArray
