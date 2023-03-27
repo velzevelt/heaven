79898d42efb42c3c54996d1dda00f6044213f261
Class_Student <Class_Student@rtk.local>
2023-03-27 09:16:45 +0300

    upd

--- scenes/objects/map_goal/objective/collect.gd
+++ scenes/objects/map_goal/objective/collect.gd
@@ -29,7 +29,7 @@ func _on_player_finished():
 
 
 func complete_check():
-	if target_count == max_target_count:
+	if target_count >= max_target_count:
 		Logger.debug_log('All collected!')
 		map_goal_data.completed = true
 		goal_completed.emit(map_goal_data, self)
