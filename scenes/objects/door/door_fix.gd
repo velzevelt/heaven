0805ff7c3775384ddcf29739ae5fc05d63c806aa
Class_Student <Class_Student@rtk.local>
2023-04-07 14:31:23 +0300

    upd

---
+++ scenes/objects/door/door_fix.gd
@@ -0,0 +1,16 @@
+extends RigidBody3D
+
+@export var joint: HingeJoint3D
+@export var fix_threshold: float = 0.4
+@export var active_layer: int = 4
+
+@onready var upper_limit: float = joint.get_param(HingeJoint3D.PARAM_LIMIT_UPPER) - fix_threshold
+@onready var lower_limit: float = joint.get_param(HingeJoint3D.PARAM_LIMIT_LOWER) + fix_threshold
+
+
+func _physics_process(_delta):
+	if (self.rotation.y > upper_limit or self.rotation.y < lower_limit) and get_contact_count() != 0:
+		self.set_collision_layer_value(active_layer, true)
+	else:
+		self.set_collision_layer_value(active_layer, false)
+
