b30081d2a7c878481816d702355e18a902ef8fb8
velzevelt <novaraptor13@gmail.com>
2023-04-08 16:56:17 +0300

    upd

--- scenes/objects/door/door.gd
+++ scenes/objects/door/door.gd
@@ -1,12 +1,16 @@
 extends RigidBody3D
 
+@export var joint: HingeJoint3D
 @export var fix_threshold: float = 0.4
 @export var active_layer: int = 4
+
+@onready var upper_limit = joint.get_param(HingeJoint3D.PARAM_LIMIT_UPPER) - fix_threshold
+@onready var lower_limit = joint.get_param(HingeJoint3D.PARAM_LIMIT_LOWER) + fix_threshold
 
 var body: PhysicsBody3D
 var raycast_data
 var is_dragging := false
-
+var product: float
 
 var normal_direction: Vector3:
 	get:
@@ -20,25 +24,24 @@ func _on_area_3d_body_exited(body):
 	self.body = null
 
 
-func _physics_process(_delta):
+func _process(_delta):
 	if is_dragging:
 		if Input.is_action_pressed('interact'):
-			push_door(raycast_data.player.global_position, 2, true)
+			pass
+			#push_door(raycast_data.player.global_position, 2, true)
 		
 		if Input.is_action_just_released('interact') or raycast_data.from.target_position.length() < (self.global_position - raycast_data.player.global_position).length():
 			is_dragging = false
 	else:
 		if is_instance_valid(body):
-			push_door(self.global_position - self.body.global_position, self.body.velocity.length() + 1)
-
-
-func push_door(push_position, push_force=1, opposite_direction=false):
-	if is_instance_valid(body):
-		var push_direction = push_position.normalized()
-		if opposite_direction:
-			push_direction = -push_direction
-		self.linear_velocity = push_direction * push_force
-
+			var push_direction = (self.global_position - body.global_position).normalized()
+			product = normal_direction.dot(body.global_position)
+			self.apply_impulse(push_direction)
+	
+	if rotation.y > upper_limit:
+		freeze = product > 0.0
+	elif rotation.y < lower_limit:
+		freeze = product < 0.0
 
 func interact_begin(data):
 	raycast_data = data
