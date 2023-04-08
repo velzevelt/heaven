70b9225a61c7486fb79e154cf14a265cf4cc075e
velzevelt <novaraptor13@gmail.com>
2023-04-08 14:32:20 +0300

    upd

--- scenes/objects/door/door.gd
+++ scenes/objects/door/door.gd
@@ -1,28 +1,24 @@
 extends RigidBody3D
 
-@export var joint: HingeJoint3D
 @export var fix_threshold: float = 0.4
 @export var active_layer: int = 4
 
-@onready var upper_limit: float = joint.get_param(HingeJoint3D.PARAM_LIMIT_UPPER) - fix_threshold
-@onready var lower_limit: float = joint.get_param(HingeJoint3D.PARAM_LIMIT_LOWER) + fix_threshold
-
-var body
+var body: PhysicsBody3D
 
 var normal_direction: Vector3:
 	get:
 		return self.transform.basis.z
 
-func _ready():
-	DebugLayer.draw.add_vector(self, 'global_position')
+#func _ready():
+#	DebugLayer.draw.add_vector(self, 'global_position')
 
 
-func _on_area_3d_body_entered(body: CharacterBody3D):
+func _on_area_3d_body_entered(body):
 	self.body = body
-
 
 func _on_area_3d_body_exited(body):
 	self.body = null
+
 
 func _physics_process(delta):
 	if is_instance_valid(body):
