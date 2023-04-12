3fe58a050c39ba68f07c819761ac6400b087ec1d
velzevelt <novaraptor13@gmail.com>
2023-04-12 18:54:20 +0300

    portal begin work

--- scenes/components/portal/cum.gd
+++ scenes/components/portal/cum.gd
@@ -1,6 +1,5 @@
 extends Camera3D
 
-@export var portal: Node3D
 @export var portal_mesh: MeshInstance3D
 @export var player_cam: Camera3D
 @export var sub_viewport: SubViewport
@@ -12,12 +11,5 @@ extends Camera3D
 	get:
 		return sub_viewport.get_texture()
 
-
-func _process(delta):
-	var m = init_global_transform * player_cam.global_transform * portal.global_transform as Transform3D
-#	m.basis = m.basis.rotated(Vector3.RIGHT, deg_to_rad(-180))
-	global_transform.basis = m.basis
-
-
 func _physics_process(_delta):
-	material.set_shader_parameter('albedo', screen_texture)
+	material.set_shader_parameter('texture_albedo', screen_texture)
