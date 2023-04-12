cba46cbe4dbe426d1235bf12fb825b931da77b88
velzevelt <novaraptor13@gmail.com>
2023-04-12 12:45:39 +0300

    upd

--- tmp/cum.gd
+++ tmp/cum.gd
@@ -1,6 +1,23 @@
 extends Camera3D
 
-@export var player: Player
+@export var portal: Node3D
+@export var portal_mesh: MeshInstance3D
+@export var player_cam: Camera3D
+@export var sub_viewport: SubViewport
+
+@onready var material: StandardMaterial3D = portal_mesh.get_active_material(0)
+@onready var init_global_transform = global_transform
+
+@onready var screen_texture: ViewportTexture = sub_viewport.get_texture():
+	get:
+		return sub_viewport.get_texture()
+
 
 func _process(delta):
-	global_position += player.global_position
+	var m = init_global_transform * player_cam.global_transform * portal.global_transform as Transform3D
+#	m.basis = m.basis.rotated(Vector3.RIGHT, deg_to_rad(90))
+	global_transform.basis = m.basis
+
+
+func _physics_process(_delta):
+	material.albedo_texture = self.screen_texture
