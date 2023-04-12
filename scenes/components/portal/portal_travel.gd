cf08b2b51ee1c613976c6df7258effee5e31f01a
velzevelt <novaraptor13@gmail.com>
2023-04-12 19:47:10 +0300

    upd

--- scenes/components/portal/portal_travel.gd
+++ scenes/components/portal/portal_travel.gd
@@ -12,3 +12,7 @@ func _on_area_3d_body_entered(body):
 		traveller = body
 		traveller.velocity = Vector3.ZERO
 		traveller.global_transform = destination.global_transform
+
+
+func _on_visible_on_screen_notifier_3d_screen_exited():
+	Logger.debug_log('123123')
