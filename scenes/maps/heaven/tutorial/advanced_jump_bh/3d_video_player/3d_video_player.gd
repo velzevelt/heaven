31808afa9d476b8c92bffe761ce898cadc71c8fb
velzevelt <novaraptor13@gmail.com>
2023-04-15 15:37:06 +0300

    upd

---
+++ scenes/maps/heaven/tutorial/advanced_jump_bh/3d_video_player/3d_video_player.gd
@@ -0,0 +1,28 @@
+extends CSGBox3D
+
+@export var video_material: StandardMaterial3D
+@onready var view_port: SubViewport = $SubViewport
+@onready var video_player: VideoStreamPlayer = $SubViewport/VideoStreamPlayer
+
+func _ready():
+	video_player.anchors_preset = Control.PRESET_FULL_RECT
+	view_port.size = get_tree().root.size
+	get_tree().root.size_changed.connect(func(): view_port.size = get_tree().root.size)
+	get_tree().root.size_changed.connect(func(): video_player.anchors_preset = Control.PRESET_FULL_RECT)
+	
+
+func _process(_delta):
+	if visible:
+		var texture = view_port.get_texture()
+		video_material.albedo_texture = texture
+
+func _on_video_stream_player_finished():
+	video_player.play()
+
+
+func _on_visible_on_screen_notifier_3d_screen_entered():
+	visible = true
+
+
+func _on_visible_on_screen_notifier_3d_screen_exited():
+	visible = false
