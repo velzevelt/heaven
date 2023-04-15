12070b2721b12941390da1260e6da37de56aa3e8
velzevelt <novaraptor13@gmail.com>
2023-04-15 16:24:03 +0300

    upd

--- scenes/maps/heaven/tutorial/advanced_jump_bh/3d_video_player/3d_video_player.gd
+++ scenes/maps/heaven/tutorial/advanced_jump_bh/3d_video_player/3d_video_player.gd
@@ -1,22 +1,15 @@
 extends MeshInstance3D
 
-@export var video_material: StandardMaterial3D
-@onready var view_port: SubViewport = $SubViewport
+@export var video_material: BaseMaterial3D
 @onready var video_player: VideoStreamPlayer = $SubViewport/VideoStreamPlayer
 
-func _ready():
-	video_player.anchors_preset = Control.PRESET_FULL_RECT
-	view_port.size = get_tree().root.size
-	get_tree().root.size_changed.connect(func(): view_port.size = get_tree().root.size)
-	get_tree().root.size_changed.connect(func(): video_player.anchors_preset = Control.PRESET_FULL_RECT)
-	
 
 func _process(_delta):
 	if visible:
-		var texture = view_port.get_texture()
+		var texture = video_player.get_video_texture()
 		video_material.albedo_texture = texture
-		texture.get_image().save_png('res://tmp/video_frame.png')
-		breakpoint
+#		texture.get_image().save_png('res://tmp/video_frame.png')
+#		breakpoint
 
 func _on_video_stream_player_finished():
 	video_player.play()
