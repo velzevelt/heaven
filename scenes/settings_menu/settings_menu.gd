2d955a32c7ed64aa69c90a5efdb9968431293f75
velzevelt <novaraptor13@gmail.com>
2023-04-16 23:39:36 +0300

    upd

--- scenes/settings_menu/settings_menu.gd
+++ scenes/settings_menu/settings_menu.gd
@@ -1,14 +1,1 @@
 extends MenuComponent
-
-
-func _on_viewport_scaling_options_item_selected(index):
-	match index:
-		0:
-			Logger.debug_log('Viewport scale mode: canvas_items')
-			get_tree().root.content_scale_mode = Window.CONTENT_SCALE_MODE_CANVAS_ITEMS
-		1:
-			Logger.debug_log('Viewport scale mode: viewport')
-			get_tree().root.content_scale_mode = Window.CONTENT_SCALE_MODE_VIEWPORT
-		_:
-			Logger.debug_log('Viewport scale mode: canvas_items')
-			get_tree().root.content_scale_mode = Window.CONTENT_SCALE_MODE_CANVAS_ITEMS
