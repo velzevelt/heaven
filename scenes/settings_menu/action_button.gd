33b0d203267dc964bb6e8838ac7857820ce0cbb5
Your Name <you@example.com>
2023-04-26 15:58:12 +0300

    upd

--- scenes/settings_menu/action_button.gd
+++ scenes/settings_menu/action_button.gd
@@ -1,13 +1,11 @@
 extends Button
 
-signal double_clicked
-signal remapped
-
 @export var linked_action := ''
 @onready var remap_menu: Control = $%RemapMenu
 
 var listen_event := false
 var new_event
+
 
 func _ready():
 	if InputMap.has_action(linked_action):
@@ -15,9 +13,6 @@ func _ready():
 	else:
 		Logger.debug_log(linked_action + " action not found", MESSAGE_TYPE.WARNING)
 	
-	double_clicked.connect(_remap_action)
-	remapped.connect(_update_text)
-	remapped.connect(func(): print(InputMap.action_get_events(linked_action)))
 
 
 func get_action_key() -> String:
@@ -37,22 +32,38 @@ func get_action_key() -> String:
 func _remap_action():
 	InputMap.action_erase_events(linked_action)
 	InputMap.action_add_event(linked_action, new_event)
-	remapped.emit()
+
 
 func _update_text():
 	text = get_action_key()
 
 
 func _gui_input(event):
+	if listen_event:
+		return
+	
 	if event is InputEventMouseButton:
 		if event.double_click:
+			await get_tree().create_timer(0.1).timeout
 			listen_event = true
-			double_clicked.emit()
 
 
 func _input(event):
-	if listen_event:
-		if event is InputEventKey or event is InputEventMouseButton:
-			if event.pressed:
-				new_event = event
-				listen_event = false
+	if not listen_event:
+		return
+
+	if event is InputEventKey:
+		if event.pressed:
+			new_event = event
+			_remap_action()
+			_update_text()
+			listen_event = false
+			
+	elif event is InputEventMouseButton:
+		if not event.double_click:
+			new_event = event
+			_remap_action()
+			_update_text()
+			listen_event = false
+	
+	
