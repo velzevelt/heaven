b839fc7ea0bf5ee55d05c97e074372ebcedce3cc
Your Name <you@example.com>
2023-04-26 15:44:50 +0300

    upd

--- scenes/settings_menu/action_button.gd
+++ scenes/settings_menu/action_button.gd
@@ -1,11 +1,13 @@
 extends Button
 
 signal double_clicked
+signal remapped
 
 @export var linked_action := ''
 @onready var remap_menu: Control = $%RemapMenu
 
 var listen_event := false
+var new_event
 
 func _ready():
 	if InputMap.has_action(linked_action):
@@ -13,36 +15,44 @@ func _ready():
 	else:
 		Logger.debug_log(linked_action + " action not found", MESSAGE_TYPE.WARNING)
 	
-#	double_clicked.connect(remap_action)
-
-
+	double_clicked.connect(_remap_action)
+	remapped.connect(_update_text)
+	remapped.connect(func(): print(InputMap.action_get_events(linked_action)))
 
 
 func get_action_key() -> String:
 	var actions = InputMap.action_get_events(linked_action)
+	var out = ''
+	
 	if actions.size() > 1:
-		var out := ''
 		for action in actions:
 			out += action.as_text() + ", "
 		out = out.rstrip(", ")
-		actions = out
-	else:
-		actions = actions[0].as_text()
+	elif actions.size() == 1:
+		out = actions[0].as_text()
 	
-	
-	return actions
+	return out
+
+
+func _remap_action():
+	InputMap.action_erase_events(linked_action)
+	InputMap.action_add_event(linked_action, new_event)
+	remapped.emit()
+
+func _update_text():
+	text = get_action_key()
 
 
 func _gui_input(event):
 	if event is InputEventMouseButton:
 		if event.double_click:
-			double_clicked.emit()
-			
-			await get_tree().physics_frame
 			listen_event = true
+			double_clicked.emit()
+
 
 func _input(event):
 	if listen_event:
 		if event is InputEventKey or event is InputEventMouseButton:
 			if event.pressed:
-				print(event.as_text())
+				new_event = event
+				listen_event = false
