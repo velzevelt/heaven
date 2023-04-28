46c502631095a8ace4d4ab1980c90178dfcaf30e
velzevelt <novaraptor13@gmail.com>
2023-04-28 18:21:41 +0300

    upd

--- scenes/autoload/events.gd
+++ scenes/autoload/events.gd
@@ -2,17 +2,18 @@ extends Node
 
 signal player_target_spotted(override_crosshair)
 signal player_target_losted(override_crosshair)
-
-
 signal player_finished
+signal goal_completed(goal_data)
 
 
 signal object_picked_up(object)
-
-signal goal_completed(goal_data)
-
 signal inventory_changed(new_inventory)
 signal item_added(new_item)
+
+
+# Sdk
+signal fullscreen_adv_requested
+
 
 # debug signals
 func _ready():
--- web_export_include/yandex_games/yandex_sdk.gd
+++ web_export_include/yandex_games/yandex_sdk.gd
@@ -1,11 +1,13 @@
-class_name YandexSdk
-extends RefCounted
+#class_name YandexSdk
+extends Node
 
 
-func _init():
+func _ready():
 	if not OS.has_feature('JavaScript'):
-		Logger.debug_log('Platform does not has JavaScript. Sdk not initialized', MESSAGE_TYPE.ERROR)
-		
+		Logger.debug_log('Platform does not has JavaScript. YandexSdk not initialized', MESSAGE_TYPE.WARNING)
+		queue_free()
+	
+	Events.fullscreen_adv_requested.connect(show_fullscreen_adv)
 
 
 func show_fullscreen_adv():
